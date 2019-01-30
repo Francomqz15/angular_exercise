c727414d24e35ccab4ac5cea3a4dabef56b62c33
Franco Marquez <fmarquez@deloitte.com>
2019-01-30 13:17:46 -0600

    add style and create form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'exercise-test';
+	title = 'exercise-test';
+
+	profileForm = new FormGroup({
+		first: new FormControl(),
+		last: new FormControl()
+	});
+
 }
